d92fab939dae9a6d5efc8fd86f4a326809db75b0
David Li <lidavid1773@gmail.com>
2022-12-30 01:28:52 -0500

    Set default plate inventory for PlatelList component

--- src/components/PlateList.js
+++ src/components/PlateList.js
@@ -1,9 +1,30 @@
 import Plate from "./Plate";
 import { useState } from "react";
 
+const redPlate = <Plate weight={55} unit={"pounds"} color={"blue"} />;
+const bluePlate = <Plate weight={45} unit={"pounds"} color={"red"} />;
+const yellowPlate = <Plate weight={35} unit={"pounds"} color={"yellow"} />;
+const greenPlate = <Plate weight={25} unit={"pounds"} color={"green"} />;
+const whitePlate = <Plate weight={10} unit={"pounds"} color={"white"} />;
+const blackPlate = <Plate weight={5} unit={"pounds"} color={"black"} />;
+
 const PlateList = () => {
-  const [inventory, setInventory] = useState([]);
-  return <div>Plate List</div>;
+  const [inventory, setInventory] = useState([
+    redPlate,
+    bluePlate,
+    yellowPlate,
+    greenPlate,
+    whitePlate,
+    blackPlate
+  ]);
+  return (
+    <div>
+      <div>Plate List</div>
+      {inventory.map((plate) => (
+        <div>{plate}</div>
+      ))}
+    </div>
+  );
 };
 
 export default PlateList;
